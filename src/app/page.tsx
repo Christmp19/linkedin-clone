7702d72ac5a6c54e7c7523b2ac4f27faea2258a5
Chritie MPASSI <christmp19@gmail.com>
2024-05-09 14:36:23 +0100

    Building the User Information Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
+import UserInformation from "@/components/UserInformation";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="grid">
+      <section>
+        {/* User information */}
+        <UserInformation />
+      </section>
+
+      <section>
+      {/* PostForm */}
+      {/* PostFeed */}
+      </section>
+
+      <section>
+      {/* Widget */}
+      </section>
       
     </main>
   );
